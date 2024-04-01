3b3348af0a6e36342d702a431bd79be43bab4341
haitianmachine <128167950+haitianmachine@users.noreply.github.com>
2024-03-31 22:29:21 -0300

    Add files via upload

---
+++ colors.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#Colors
+export WHITE=0xffffffff
+export GREEN=0xff76ed5c
+export RED=0xffff4242
+export BLUE=0xff4f82f4
+export TRANSPARENT=0x00000000
+export MAGENTA=0xffc6a0f6
+export GRAY=0xff1e1e25
+
+# -- Teal Scheme --
+# export BAR_COLOR=0xff001f30
+# export ITEM_BG_COLOR=0xff003547
+# export ACCENT_COLOR=0xff2cf9ed
+
+# -- Gray Scheme --
+# export BAR_COLOR=0xff101314
+# export ITEM_BG_COLOR=0xff353c3f
+# export ACCENT_COLOR=0xffffffff
+
+# -- Purple Scheme --
+# export BAR_COLOR=0xff140c42
+# export ITEM_BG_COLOR=0xff2b1c84
+# export ACCENT_COLOR=0xffeb46f9
+
+# -- Red Scheme ---
+# export BAR_COLOR=0xff23090e
+# export ITEM_BG_COLOR=0xff591221
+# export ACCENT_COLOR=0xffff2453
+
+# -- Blue Scheme ---
+# export BAR_COLOR=0xff021254
+# export ITEM_BG_COLOR=0xff093aa8
+# export ACCENT_COLOR=0xff15bdf9
+
+# -- Green Scheme --
+# export BAR_COLOR=0xff003315
+# export ITEM_BG_COLOR=0xff008c39
+# export ACCENT_COLOR=0xff1dfca1
+
+
+# -- Orange Scheme --
+# export BAR_COLOR=0xff381c02
+# export ITEM_BG_COLOR=0xff99440a
+# export ACCENT_COLOR=0xfff97716
+
+# -- Yellow Scheme --
+# export BAR_COLOR=0xff2d2b02
+# export ITEM_BG_COLOR=0xff8e7e0a
+# export ACCENT_COLOR=0xfff7fc17
+
+
+# -- Sonoma Hills Night (Background) Scheme --
+ # export BAR_COLOR=0xff223563
+ # export ITEM_BG_COLOR=0xff223563
+ # export ACCENT_COLOR=0xff4e83aa
+
+
+ # -- Gray Scheme (No Left Background) --
+ # export BAR_COLOR=$GRAY
+
+
+ export BAR_COLOR=0xff101314
+
+
+ # export ITEM_BG_COLOR=0xff24273a
+ export ITEM_BG_COLOR=0xff5b5b5b
+ export ACCENT_COLOR=0xffffffff
